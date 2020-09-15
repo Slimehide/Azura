42b182894565fa51bcda4ecab1b1cdcb42bbc02b
Nikitronus <Slimehide@gmail.com>
2020-09-15 21:43:47 +0300

    Some more enchantments

---
+++ js/script.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+	$(".menu__button").on("click" ,function(e){
+		if ($(this).hasClass("active__menu")) {
+			$("body,html").css("overflow-y" ,"auto");
+			$(".main__menu").css("top" ,"-100vh");
+			$(this).removeClass("active__menu");
+			if ($(window).width() < 640) {
+				$(".header__buttons").css("top" , "-100vh");
+			}
+		} else {
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+			$(".main__menu").css("top" ,"0px");
+			$(this).addClass("active__menu");
+			$("body,html").css("overflow-y" ,"hidden");
+			if ($(window).width() < 640) {
+				$(".header__buttons").css("top" , "0");
+			}
+		}
+	});
+
+	var getUrlParameter = function getUrlParameter(sParam) {
+	    var sPageURL = window.location.search.substring(1),
+	        sURLVariables = sPageURL.split('&'),
+	        sParameterName,
+	        i;
+	    for (i = 0; i < sURLVariables.length; i++) {
+	        sParameterName = sURLVariables[i].split('=');
+
+	        if (sParameterName[0] === sParam) {
+	            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+	        }
+	    }
+	};
+	function setQueryStringParameter(name, value) {
+	    const params = new URLSearchParams(window.location.search);
+	    params.set(name, value);
+	    window.history.replaceState({}, "", decodeURIComponent(`${window.location.pathname}?${params}`));
+	}
+	function updateUrlParameter(param, value) {
+	    const regExp = new RegExp(param + "(.+?)(&|$)", "g");
+	    const newUrl = window.location.href.replace(regExp, param + "=" + value + "$2");
+	    window.history.pushState("", "", newUrl);
+	}
+	function clearHistory(){
+		window.history.pushState({}, document.title, window.location.protocol + "//" + window.location.host + window.location.pathname);
+	}
+
+	var blockPath = getUrlParameter('block');
+	if (blockPath != undefined) {
+		setTimeout(function(){
+			if (blockPath == "home") {
+				$('html').animate({ 
+		            scrollTop: $("#" + blockPath).offset().top - 90
+		        }, 500 );
+			} else {
+				$('html').animate({ 
+		            scrollTop: $("#" + blockPath).offset().top + 20
+		        }, 500 );
+			}
+			
+		} ,300);		
+	}
+
+
+	$('.first__button>a').on("click" , function(e){
+		$('html').animate({ 
+	            scrollTop: $("#about").offset().top + 20
+	        }, 900 );
+	});
+
+	$('.more__world>a').on("click" , function(e){
+		$('html').animate({ 
+	            scrollTop: $(".ad__block").offset().top
+	        }, 900 );
+	});
+
+	$(".scrollable__button").on("click" ,function(e){
+		e.preventDefault();
+		setQueryStringParameter("block" , $(this).attr("data-block"));
+		if ($("#" + $(this).attr("data-block")).length) {
+			if ($(window).width() < 991) {
+				$("body,html").css("overflow-y" ,"auto");
+				$(".main__menu").css("top" ,"-100vh");
+				$(".menu__button").removeClass("active__menu");
+				$(this).removeClass("active__menu");
+			}
+			if ($(window).width() < 640) {
+				$(".header__buttons").css("top" , "-100vh");
+			}
+			if ($(this).attr("data-block") == "home") {
+				$('html').animate({ 
+		            scrollTop: $("#" + $(this).attr("data-block")).offset().top -84
+		        }, 900 );
+			} else{
+				$('html').animate({ 
+		            scrollTop: $("#" + $(this).attr("data-block")).offset().top  + 20
+		        }, 900 );
+			}
+			
+		} else {
+			window.location.href = "index.html?block=" + $(this).attr("data-block");
+		}
+		
+	});
+});
